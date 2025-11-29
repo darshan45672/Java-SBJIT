45d49e0feae4c50581ce9f7e53b03953b8d74d0b
darshan45672 <drshnbhandary@gmail.com>
2025-11-29 12:18:42 +0530

    done

---
+++ dataTypes.java
@@ -0,0 +1,33 @@
+// Data Types in Java
+
+class DataTypes{
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        byte byteVar1 = 100;
+        byte byteVar2 = 010;
+
+        short shortVar1 = 10000;
+        short shortVar2 = 02000;
+
+        long longVar1 = 100000L;
+        long longVar2 = 020000L;
+
+        float floatVar1 = 10.5f;
+        float floatVar2 = 20.0f;
+
+        double doubleVar1 = 100.99;
+        double doubleVar2 = 200.99;
+
+        System.out.println("Integer Addition: " + (num1 + num2));
+        System.out.println("Byte Addition: " + (byteVar1 + byteVar2));
+        System.out.println("Short Addition: " + (shortVar1 + shortVar2));
+        System.out.println("Long Addition: " + (longVar1 + longVar2));
+        System.out.println("Float Addition: " + (floatVar1 + floatVar2));
+        System.out.println("Double Addition: " + (doubleVar1 + doubleVar2));
+
+
+    }
+}
--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
+
+import java.util.*;
+
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        scanner.close();
     }
 }
