c92ef81326e6f4268bab54fa619c5211079bf432
darshan45672 <drshnbhandary@gmail.com>
2025-11-29 12:37:16 +0530

    wrapper

---
+++ Wrapper.java
@@ -0,0 +1,17 @@
+// Program for Wrapper class in Java
+
+public class Wrapper{
+    public static void main(String[] args) {
+        Double d1 = Double.valueOf(10.5);
+        Double d2 = Double.valueOf(10);
+        Double d3 = 10.5;
+        Double d4 = 10;
+        Float f1 = 12;
+
+        System.out.println("d1: " + d1);
+        System.out.println("d2: " + d2);
+        System.out.println("d3: " + d3);
+        System.out.println("d4: " + d4);
+        
+    }
+}
