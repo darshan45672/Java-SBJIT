dc9fbf22a0afd2b0cbfff8c74115075c4cc14977
darshan45672 <drshnbhandary@gmail.com>
2025-12-01 10:28:59 +0530

    assignment1

---
+++ Assignement1.java
@@ -0,0 +1,7 @@
+// Vasanth wants to go to a trip with his girlfriend, but he has a budget issue
+// if his budget is less than 1000 he can take his girlfiend to a nearby place
+// if his budget is between 1000 and 3000 he can take her to a hill station
+// if his budget is between 3000 and 5000 he can take her to a beach
+// if his budget is more than 50000 he can take her abroad.
+// write a java code to help vasanth to decide where he can take his girlfriend 
+// based on his budget.
