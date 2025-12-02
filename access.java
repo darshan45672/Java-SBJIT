0625a776f4927b5ab91cfed911d42f3ab187c434
darshan45672 <drshnbhandary@gmail.com>
2025-12-02 12:25:45 +0530

    add getter and setter methods for name and age in Access class

--- access.java
+++ access.java
@@ -13,4 +13,23 @@ class Access {
     public int getAge() {
         return age;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Access person = new Access("John", 30);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+
+        person.setName("Jane");
+        person.setAge(25);
+
+        System.out.println("Updated Name: " + person.getName());
+        System.out.println("Updated Age: " + person.getAge());
+    }
 }
