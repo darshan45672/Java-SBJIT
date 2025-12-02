88a6aaf58a6f071a04bb4ee45f487fb6a6f0ddb5
darshan45672 <drshnbhandary@gmail.com>
2025-12-02 11:59:36 +0530

    add Private class with secret number and access methods

---
+++ private.java
@@ -0,0 +1,18 @@
+class Private {
+    private int secretNumber = 42;
+
+    private void revealSecret() {
+        System.out.println("The secret number is: " + secretNumber);
+    }
+
+    public void accessSecret() {
+        revealSecret();
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Private myPrivate = new Private();
+            myPrivate.accessSecret();
+        }
+    }
+}
