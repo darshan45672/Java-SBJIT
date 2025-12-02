25191e4229ea1851e7da10e38a1aa8fe0fab790a
darshan45672 <drshnbhandary@gmail.com>
2025-12-02 12:24:39 +0530

    add Access class with private fields and access methods

---
+++ access.java
@@ -0,0 +1,16 @@
+class Access {
+    private int age;
+    private String name;
+
+    public Access(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+}
