f852af08044bf63f7ef9e7c7781748b8005a2ad1
darshan45672 <drshnbhandary@gmail.com>
2025-12-02 14:37:58 +0530

    add Abst class with nested Animal and Dog classes to implement basic animal behavior

---
+++ abst.java
@@ -0,0 +1,24 @@
+public Class Abst {
+    public abstract class Animal {
+        public abstract void makeSound();
+
+        public void sleep() {
+            System.out.println("Zzz...");
+        }
+    }
+
+    public class Dog extends Animal {
+        @Override
+        public void makeSound() {
+            System.out.println("Woof Woof");
+        }
+    }
+
+    public static void main(String[] args) {
+        Abst abst = new Abst();
+        Dog myDog = abst.new Dog();
+        myDog.makeSound();
+        myDog.sleep();
+        myDog.super.sleep();
+    }
+}
