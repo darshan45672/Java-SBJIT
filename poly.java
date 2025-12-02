75905cde7f368b72780adf8b0b82a1de9a16b9f6
darshan45672 <drshnbhandary@gmail.com>
2025-12-02 14:25:23 +0530

    refactor Poly and Quad classes to improve display method functionality and streamline main method

--- poly.java
+++ poly.java
@@ -2,19 +2,25 @@ public class Poly{
     public void display(int number){
         System.out.println("Displaying integer: " + number);
     }
+}
+
+public class Quad extends Poly{
+    @Override
+    public void display(int number){
+        System.out.println("Displaying integer in Quad class: " + number);
+    }
 
     public void display(String text){
         System.out.println("Displaying string: " + text);
     }
 
-    public void display(double decimal){
-        System.out.println("Displaying double: " + decimal);
+    public static void main(String[] args) {
+        Poly polyObj = new Poly();
+        polyObj.display(10);
+
+        Quad quadObj = new Quad();
+        quadObj.display(20);
+        quadObj.display("Hello, Polymorphism!");
     }
 
-    public static void main(String[] args) {
-        Poly poly = new Poly();
-        poly.display(10);
-        poly.display("Hello, World!");
-        poly.display(3.14);
-    }
 }
